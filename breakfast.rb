367d2d8ad71a8954e278ade972d898011b0d411a
shinju0514 <shinju0514yellow@gmail.com>
2022-06-19 05:17:46 +0000

    ruby practice

---
+++ breakfast.rb
@@ -0,0 +1,11 @@
+breakfast = 'ご飯'
+
+case breakfast
+when 'パン' then
+  puts 'パン派です'
+when 'ご飯' then
+  puts 'ご飯派です'
+else
+  puts 'パンはでもご飯派でもありません'
+end
+
--- type.rb
+++ type.rb
@@ -1,3 +1,3 @@
-puts 5 + 3
+puts 5.1 + 3.2
 puts "5 + 3"
 puts "5" + "3"
