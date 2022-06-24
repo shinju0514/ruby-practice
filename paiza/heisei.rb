5dab6c571c6277b0b6b4cae9542ac52450b47b28
shinju0514 <shinju0514yellow@gmail.com>
2022-06-24 14:53:42 +0000

    heisei

---
+++ paiza/heisei.rb
@@ -0,0 +1,8 @@
+seireki = 2015
+puts "西暦#{seireki}年は、"
+
+# 平成1年とは、西暦1989年。その差は、1988
+# 西暦年 - 1988 = 平成*年
+
+heisei = seireki - 1988
+puts "平成#{heisei}年です。"
---
+++ paiza/rand.rb
@@ -0,0 +1,6 @@
+# if文による条件分岐
+number = rand(1..3) * 100
+puts "あなたの得点は#{number}ポイントです"
+if number == 300
+    puts "おめでとう！"
+end
