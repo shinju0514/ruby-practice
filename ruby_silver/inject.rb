de23b6ecd3fbab067a314d9b3df72e3b4b77569c
shinju0514 <shinju0514yellow@gmail.com>
2022-06-27 13:46:55 +0000

    inject

--- ruby_silver/inject.rb
+++ ruby_silver/inject.rb
@@ -1,3 +1,14 @@
 array = 1..6
 
 array.inject (0){ |sum,num| p sum += num}
+
+array = 1..6
+
+#以下のinjectと同等の処理をeachで実行します。
+#array.inject (0){ |sum,num| p sum+=num}
+
+sum = 0
+array.each do |num|
+  sum += num
+  p sum
+end
