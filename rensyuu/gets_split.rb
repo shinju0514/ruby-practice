5491fe5bcd7d2338dad89a0ca3765eb547947d1f
shinju0514 <shinju0514yellow@gmail.com>
2022-07-09 02:12:30 +0000

    practice

---
+++ rensyuu/gets_split.rb
@@ -0,0 +1,7 @@
+
+# 標準入力
+line = gets.split(' ')
+p line
+
+# 出力結果
+["Tokyo", "Osaka", "Kyoto"]
---
+++ rensyuu/method.rb
@@ -0,0 +1,7 @@
+# def メソッドの名前(引数1、引数2…)
+  # 処理
+# end
+
+def hello(name, age)
+  puts "Hello World! #{name}! You are #{age} years old!"
+end
