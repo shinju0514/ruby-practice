a36bfaac81c87e7d4bbd8c1b89065673e87d7406
shinju0514 <shinju0514yellow@gmail.com>
2022-07-11 07:39:38 +0000

    practice

--- ruby_silver/suuretu.rb
+++ ruby_silver/suuretu.rb
@@ -1,15 +1,18 @@
-N = gets.to_i
-X = gets.to_i
-Y = gets.to_i
 
-def tousen
-    if  X % N == 0
-        puts 'A'
-    elsif Y % N == 0
-        puts 'B'
-    elsif (X % N == 0) || (Y % N == 0)
+i = 1
+# gets.split(' ')で一行で入力した値を配列で返す。
+# map(&:to_i)で配列に格納した文字列を整数型に変換する。
+H,U,K = gets.split(' ').map(&:to_i)
+
+while H >= i do
+    if  ( i % U == 0 ) && ( i % K == 0 )
         puts 'AB'
+    elsif i % U == 0
+        puts 'A'
+    elsif i % K == 0
+        puts 'B'
     else
         puts 'N'
     end
+    i += 1
 end
