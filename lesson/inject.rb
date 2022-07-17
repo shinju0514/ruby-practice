a69f3c721d99ddc948a121a376a1aee12618a344
shinju0514 <shinju0514yellow@gmail.com>
2022-07-17 15:13:40 +0000

    inject

---
+++ lesson/inject.rb
@@ -0,0 +1,10 @@
+p [1, 2, 3].inject{|x, y| x + y ** 2} rescue p $!
+#=> 14
+
+p [1, 2, 3].inject(0){|x, y| x + y ** 2} rescue p $!
+#=> 14
+
+p [1, 2, 3].inject([]){|x, y| x << y ** 2} rescue p $!
+#=> [1, 4, 9]
+
+p [1, 2, 3].inject do|x, y| x + y ** 2 end rescue p $!
