81d5246a9add28793050f69eb26c5941a1026325
shinju0514 <shinju0514yellow@gmail.com>
2022-08-17 13:33:29 +0000

    hand

---
+++ ruby_silver/hand.rb
@@ -0,0 +1,21 @@
+hand = "グー"
+if hand == "グー"
+  puts "出した手はグーです"
+end
+
+if hand != "チョキ"
+  puts "出した手はチョキではありません"
+end
+
+if (hand == "グー") || (hand == "パー")
+  puts "出した手はグーまたはパーです"
+end
+
+apple = "Yamanashi"
+if apple == "Aomori"
+  puts "このリンゴは青森県産です。"
+elsif apple == "Nagano"
+  puts "このリンゴは青森県産ではなく、長野県産です。"
+else
+  puts "このリンゴは青森県産でも長野県産でもありません。"
+end
